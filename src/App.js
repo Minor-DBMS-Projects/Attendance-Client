cc6ca90247ae0b6aaf3530ead39b8dc733ca8cd3
Pranab <mpranab7890@gmail.com>
2020-11-11 22:43:07 +0545

    Modified routes and added authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+  Redirect,
+} from "react-router-dom";
+import axios from "axios";
 import Header from "./components/layout/Header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Class from "./components/Class";
 
-function App() {
+function App(props) {
+  let history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
+  useEffect(() => {
+    fetch(
+      "/authentication"
+      // , {
+      //   method: "GET",
+      //   credentials: "include",
+      //   headers: {
+      //     Accept: "application/json",
+      //     "Content-Type": "application/json",
+      //     "Access-Control-Allow-Credentials": true,
+      //   },
+      // }
+    )
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        //throw new Error("failed to authenticate user");
+      })
+      .then((responseJson) => {
+        if (responseJson.authenticated) {
+          setAuthenticated(true);
+        } else {
+          setAuthenticated(false);
+        }
+      })
+      .catch((error) => {
+        setAuthenticated(false);
+        console.log(error);
+      });
+  }, []);
+
   return (
     <Router>
       <div className="container-fluid">
         <Header />
+        <Switch>
+          <Route exact path="/">
+            {authenticated ? (
+              <Redirect to="/dashboard" />
+            ) : (
+              <Login
+                authenticated={authenticated}
+                setAuthenticated={setAuthenticated}
+              />
+            )}
+          </Route>
+          <Route exact path="/dashboard">
+            {authenticated ? <Dashboard /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/classes">
+            {authenticated ? <Class /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/success">
+            <h1>Logged in successfully</h1>
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
