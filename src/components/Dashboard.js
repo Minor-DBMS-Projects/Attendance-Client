c3b555cbc07c02786537775e36db8b1f3c468f1e
Pranab <mpranab7890@gmail.com>
2020-11-13 13:24:51 +0545

    Simple dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter, Redirect } from "react-router-dom";
+import axios from "axios";
+import {
+  MDBContainer,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+} from "mdbreact";
+import "../App.css";
 
 const Dashboard = () => {
+  const [attendanceData, setattendanceData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/attendance/getRecent/30")
+      .then((res) => {
+        console.log(res.data);
+        setattendanceData(res.data);
+      })
+      .catch((err) => console.log(err));
+    // fetch("/attendance/getRecent/30")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setattendanceData(data);
+    //   });
+    return () => console.log("unmounting...");
+  }, [setattendanceData]);
   return (
-    <div>
-      <h1>Recent attendances</h1>
-    </div>
+    <MDBContainer>
+      <hr />
+      <hr />
+      <h4>My Classes</h4>
+      <hr />
+      <MDBListGroup className=" ">
+        {attendanceData.map((attendance) => (
+          <div className="child">
+            <MDBListGroupItem hover href="#">
+              <div
+                className="d-flex w-100 justify-content-between"
+                style={{ color: "#007bff" }}
+              >
+                <h5 className="mb-1">{attendance.subject}</h5>
+                <small>{attendance.program}</small>
+              </div>
+
+              <p className="mb-1">
+                <strong>Program</strong>: {attendance.program}
+                <br />
+                <strong>Batch</strong>: {attendance.batch}
+                <br />
+                <strong>Section</strong>: {attendance.section}
+              </p>
+              <small>Donec id elit non mi porta.</small>
+            </MDBListGroupItem>
+          </div>
+        ))}
+      </MDBListGroup>
+    </MDBContainer>
   );
 };
 
