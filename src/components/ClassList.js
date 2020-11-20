b458ee7a82214016ff96f836aa4b6df6bb7dd0e9
Pranab <mpranab7890@gmail.com>
2020-11-20 14:12:27 +0545

    Passed the props via link to AttendanceHistory component

--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -4,7 +4,6 @@ import "../App.css";
 import { withRouter, Redirect, Link } from "react-router-dom";
 import axios from "axios";
 
-
 const ClassList = (props) => {
   //const [takingAttendance,settakingAttendance]=useState(false)
   //const [postData,setpostData]=useState({})
@@ -18,17 +17,17 @@ const ClassList = (props) => {
       `/history/${props.attendance.class}/${props.attendance.subjectCode}/${props.attendance.classType}`
     );
   };
-  const handleSubmit=(event)=>{
-    event.preventDefault()
-    const details={
-      'batch':props.attendance.batch,
-      'classid':props.attendance.class,
-      'program':props.attendance.program,
-      'section':props.attendance.section,
-      'subjectcode':props.attendance.subjectCode,
-      'subject':props.attendance.subject,
-      'type':props.attendance.classType
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const details = {
+      batch: props.attendance.batch,
+      classid: props.attendance.class,
+      program: props.attendance.program,
+      section: props.attendance.section,
+      subjectcode: props.attendance.subjectCode,
+      subject: props.attendance.subject,
+      type: props.attendance.classType,
+    };
     var formBody = [];
     for (var property in details) {
       var encodedKey = encodeURIComponent(property);
@@ -36,71 +35,73 @@ const ClassList = (props) => {
       formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    axios.post('/student/namelist',formBody,{
+    axios
+      .post("/student/namelist", formBody, {
         headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        
-      }})
-      .then(response=>{
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+      })
+      .then((response) => {
         props.history.push({
-          pathname:'/new/student/namelist',
-          state:{
-            data:response.data
-          }
-        })
+          pathname: "/new/student/namelist",
+          state: {
+            data: response.data,
+          },
+        });
         //settakingAttendance(true)
         //setpostData(response.data)
       })
-      .catch(errorMsg=>console.log(errorMsg.response))
-  
-  }
- // if(takingAttendance){
- //   settakingAttendance(false)
- //   
- //   return(
- //     props.history.push({
- //       pathname:'/new/student/namelist',
- //       state:{data:postData}
- //     })
- //     <Redirect
- //           to={{
- //           pathname: "/new/student/namelist",
- //           state: { data: postData }
- //         }}
- //       />
- //     
- //   )
- // }
+      .catch((errorMsg) => console.log(errorMsg.response));
+  };
+  // if(takingAttendance){
+  //   settakingAttendance(false)
+  //
+  //   return(
+  //     props.history.push({
+  //       pathname:'/new/student/namelist',
+  //       state:{data:postData}
+  //     })
+  //     <Redirect
+  //           to={{
+  //           pathname: "/new/student/namelist",
+  //           state: { data: postData }
+  //         }}
+  //       />
+  //
+  //   )
+  // }
   return (
     <div className="child">
       <MDBListGroupItem hover>
-      <form onSubmit={handleSubmit}>
-        <div
-          className="d-flex w-100 justify-content-between"
-          style={{ color: "#007bff" }}
-        >
-        
-          <h5 className="mb-1">{props.attendance.subject}</h5>
-          <small>{props.attendance.program}</small>
-        </div>
-        <p className="mb-1">
-          <strong>Program</strong>: {props.attendance.program}
-          <br />
-          <strong>Batch</strong>: {props.attendance.batch}
-          <br />
-          <strong>Section</strong>: {props.attendance.section}
-        </p>
-        <Link
-          to={`/history/${props.attendance.class}/${props.attendance.subjectCode}/${props.attendance.classType}`}
-        >
-          <MDBBtn color="primary" style={{ color: "white" }}>
-            Attendance history
-          </MDBBtn>
-        </Link>
-        <MDBBtn color="primary" type="submit" style={{ color: "white" }}>
+        <form onSubmit={handleSubmit}>
+          <div
+            className="d-flex w-100 justify-content-between"
+            style={{ color: "#007bff" }}
+          >
+            <h5 className="mb-1">{props.attendance.subject}</h5>
+            <small>{props.attendance.program}</small>
+          </div>
+          <p className="mb-1">
+            <strong>Program</strong>: {props.attendance.program}
+            <br />
+            <strong>Batch</strong>: {props.attendance.batch}
+            <br />
+            <strong>Section</strong>: {props.attendance.section}
+          </p>
+          <Link
+            to={{
+              pathname: `/history/${props.attendance.class}/${props.attendance.subjectCode}/${props.attendance.classType}`,
+              state: { attendance: props.attendance },
+            }}
+          >
+            <MDBBtn color="primary" style={{ color: "white" }}>
+              Attendance history
+            </MDBBtn>
+          </Link>
+          <MDBBtn color="primary" type="submit" style={{ color: "white" }}>
             Take Attendance
           </MDBBtn>
-          </form>
+        </form>
       </MDBListGroupItem>
     </div>
   );
