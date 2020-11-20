828083c92d77a39ea506f409921b41cc2dd2f546
Pranab <mpranab7890@gmail.com>
2020-11-20 14:10:53 +0545

    Hidden the header in history page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,20 +7,20 @@ class Header extends Component {
   j;
   render() {
     const { location } = this.props;
-    // if (location.pathname.match("/history")) return null;
-    // else
-    return (
-      <div className="bg-primary z-depth-3">
-        <h1 className=" p-3 d-flex justify-content-center text-white">
-          Institute of Engineering
-        </h1>
-        <h4 className="mt-1 p-0 d-flex justify-content-center text-white">
-          Pulchowk Campus
-        </h4>
+    if (location.pathname.match("/history")) return <Navigation />;
+    else
+      return (
+        <div className="bg-primary z-depth-3">
+          <h1 className=" p-3 d-flex justify-content-center text-white">
+            Institute of Engineering
+          </h1>
+          <h4 className="mt-1 p-0 d-flex justify-content-center text-white">
+            Pulchowk Campus
+          </h4>
 
-        <Navigation />
-      </div>
-    );
+          <Navigation />
+        </div>
+      );
   }
 }
 
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -21,7 +21,7 @@ export default class Navigation extends Component {
 
   render() {
     return (
-      <div className="mt-5" style={{ backgroundColor: "#007bff" }}>
+      <div className="" style={{ backgroundColor: "#007bff" }}>
         <MDBNavbar dark expand="md">
           <MDBNavbarToggler onClick={this.toggleCollapse} />
           <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
