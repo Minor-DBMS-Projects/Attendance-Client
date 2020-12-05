83dd9e6cd1ee8766ab526ae944d1c86e3f4300ea
Pranab <mpranab7890@gmail.com>
2020-12-05 19:22:48 +0545

    Removed navigation when not logged in

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,90 +1,87 @@
 import React, { Component } from "react";
 import {
-    MDBNavbar,
-    MDBNavbarNav,
-    MDBNavItem,
-    MDBNavLink,
-    MDBNavbarToggler,
-    MDBCollapse,
-    MDBIcon,
-    MDBBtn,
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBIcon,
+  MDBBtn,
 } from "mdbreact";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 class Navigation extends Component {
-    state = {
-        isOpen: false,
-    };
+  state = {
+    isOpen: false,
+  };
 
-    toggleCollapse = () => {
-        this.setState({ isOpen: !this.state.isOpen });
-    };
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
-    logout = () => {
-        this.props.setloading(true);
-        axios
-            .get("/backend/logout")
-            .then(() => {
-                this.props.setAuthenticated(!this.props.authenticated);
-                this.props.history.push("/");
-                this.props.setloading(false);
-            })
-            .catch((err) => console.log(err));
-    };
+  logout = () => {
+    this.props.setloading(true);
+    axios
+      .get("/backend/logout")
+      .then(() => {
+        this.props.setAuthenticated(!this.props.authenticated);
+        this.props.history.push("/");
+        this.props.setloading(false);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    render() {
-        return (
-            <div className="" style={{ backgroundColor: "#007bff" }}>
-                <MDBNavbar dark expand="md">
-                    <MDBNavbarToggler onClick={this.toggleCollapse} />
-                    <MDBCollapse
-                        id="navbarCollapse3"
-                        isOpen={this.state.isOpen}
-                        navbar
-                    >
-                        <MDBNavbarNav left>
-                            <MDBBtn color="primary">
-                                <MDBNavItem>
-                                    <MDBNavLink to="/" exact>
-                                        Home
-                                    </MDBNavLink>
-                                </MDBNavItem>
-                            </MDBBtn>
-                            <MDBBtn color="primary">
-                                <MDBNavItem>
-                                    <MDBNavLink to="/class-details" exact>
-                                        Take Attendance
-                                    </MDBNavLink>
-                                </MDBNavItem>
-                            </MDBBtn>
-                            <MDBBtn color="primary">
-                                <MDBNavItem>
-                                    <MDBNavLink to="/online-attendance">
-                                        Online Class
-                                    </MDBNavLink>
-                                </MDBNavItem>
-                            </MDBBtn>
-                            <MDBBtn color="primary">
-                                <MDBNavItem>
-                                    <MDBNavLink to="/batch">Batch</MDBNavLink>
-                                </MDBNavItem>
-                            </MDBBtn>
-                        </MDBNavbarNav>
-                        {this.props.authenticated ? (
-                            <MDBNavbarNav right>
-                                <MDBNavItem>
-                                    <MDBBtn onClick={this.logout}>
-                                        <MDBNavLink to=""> Logout</MDBNavLink>
-                                    </MDBBtn>
-                                </MDBNavItem>
-                            </MDBNavbarNav>
-                        ) : null}
-                    </MDBCollapse>
-                </MDBNavbar>
-            </div>
-        );
+  render() {
+    if (!this.props.authenticated) {
+      return <br />;
     }
+    return (
+      <div className="" style={{ backgroundColor: "#007bff" }}>
+        <MDBNavbar dark expand="md">
+          <MDBNavbarToggler onClick={this.toggleCollapse} />
+          <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+            <MDBNavbarNav left>
+              <MDBBtn color="primary">
+                <MDBNavItem>
+                  <MDBNavLink to="/" exact>
+                    Home
+                  </MDBNavLink>
+                </MDBNavItem>
+              </MDBBtn>
+              <MDBBtn color="primary">
+                <MDBNavItem>
+                  <MDBNavLink to="/class-details" exact>
+                    Take Attendance
+                  </MDBNavLink>
+                </MDBNavItem>
+              </MDBBtn>
+              <MDBBtn color="primary">
+                <MDBNavItem>
+                  <MDBNavLink to="/online-attendance">Online Class</MDBNavLink>
+                </MDBNavItem>
+              </MDBBtn>
+              <MDBBtn color="primary">
+                <MDBNavItem>
+                  <MDBNavLink to="/batch">Batch</MDBNavLink>
+                </MDBNavItem>
+              </MDBBtn>
+            </MDBNavbarNav>
+            {this.props.authenticated ? (
+              <MDBNavbarNav right>
+                <MDBNavItem>
+                  <MDBBtn onClick={this.logout}>
+                    <MDBNavLink to=""> Logout</MDBNavLink>
+                  </MDBBtn>
+                </MDBNavItem>
+              </MDBNavbarNav>
+            ) : null}
+          </MDBCollapse>
+        </MDBNavbar>
+      </div>
+    );
+  }
 }
 
 export default withRouter(Navigation);
